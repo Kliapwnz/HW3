8c8fb66a03061c99cc4e10de9c9a5e495405707f
Evgeniy Klimovch <kliasanik@gmail.com>
2023-01-31 14:03:43 +0300

    hw5

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
@@ -19,12 +19,13 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*<Route  path={'...'} element={<Navigate to={...} />}*/}
-
+                <Route path={"/"} element={<Navigate to={"/pre-junior"}/>}/>
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route path={...} element={<PreJunior />}*/}
+                <Route path={"/pre-junior"} element={<PreJunior/>}/>
+                <Route path={"/junior"} element={<Junior/>}/>
+                <Route path={"/junior-plus"} element={<JuniorPlus/>}/>
                 {/*<Route ...*/}
-                {/*<Route ...*/}
-
+                <Route path={"/..."} element={<Error404/>}/>
                 {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
                 {/*<Route ...*/}
             </Routes>
